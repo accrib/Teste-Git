4c1234cfdc6875eb9e6c3dafa409c8ea3a0f2931
Catarina Ribeiro <ana.catarina.c.ribeiro@gmail.com>
2020-04-11 15:53:58 +0100

    11/04/2020 - v1 - Adicionada a Classe_1

---
+++ Git/Classe_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git
+{
+    public class Classe_1
+    {
+        public static void Primeira_Classe()
+        {
+            Console.WriteLine("Esta é a minha primeira classe.");
+        }
+    }
+}
---
+++ Git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+
+            Classe_1.Primeira_Classe();
+        }
+    }
+}
